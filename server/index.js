3fd849e05ec6f05bc9038f89eb6b0c5ebd404b7a
JasonWen24 <jasonwendavidson@gmail.com>
2023-05-17 13:56:18 +0200

    chore: put mongo address in env file

--- server/index.js
+++ server/index.js
@@ -1,10 +1,22 @@
-const express = require('express');
-const app = express();
-const mongoose = require('mongoose');
+import express from "express"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
 
-mongoose.connect('mongodb+srv://admin:test123@stutor.nen5zpi.mongodb.net/StuTor?retryWrites=true&w=majority');
+
+const app = express();
+dotenv.config();
+
+const connect = async() =>{
+    try{
+        await mongoose.connect(process.env.MONGO)
+        console.log("*********** CONNECTED TO MONGODB ***********")
+    } catch (error){
+        console.log(error);
+    }
+};
 
 const port = 3001;
 app.listen(port, () => {
+    connect()
     console.log('server running on port ' + port);
 });
