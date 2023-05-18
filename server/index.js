545a626e3ebc63876c32a1ac5d9e1f44a5779f29
Sophie <sophie.witt@tum.de>
2023-05-18 16:11:16 +0200

    Add routers to routes

--- server/index.js
+++ server/index.js
@@ -2,6 +2,12 @@ import express from "express"
 import mongoose from "mongoose"
 import dotenv from "dotenv"
 import userRoute from "./routes/user.route.js"
+import bookingRoute from "./routes/booking.route.js"
+import courseRoute from "./routes/course.route.js"
+import achievementRoute from "./routes/achievement.route.js"
+import studysessionRoute from "./routes/studysession.route.js"
+import universityRoute from "./routes/university.route.js"
+import reviewRoute from "./routes/review.route.js"
 
 
 const app = express();
@@ -17,7 +23,12 @@ const connect = async() =>{
 };
 
 app.use("/api/user", userRoute);
-
+app.use("api/booking", bookingRoute);
+app.use("api/course", courseRoute);
+app.use("api/achievement", achievementRoute);
+app.use("api/studysession", studysessionRoute);
+app.use("api/university", universityRoute);
+app.use("api/review", reviewRoute);
 
 const port = 3001;
 app.listen(port, () => {
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -6,7 +6,4 @@ const router = Express.Router();
 router.route('/').get(getUser);
 router.route('/').delete(deleteUser);
 
-router.get('/test', (req, res) => {
-    res.send('Hello World!')});
-
 export default router;
