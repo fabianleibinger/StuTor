0a035fbe5028dddcbfa4737dd9b91121b096d314
Sophie <sophie.witt@tum.de>
2023-05-18 17:46:45 +0200

    Change import schema

--- server/models/Booking.js
+++ server/models/Booking.js
@@ -1,4 +1,5 @@
-import Schema from 'mongoose';
+import mongoose from "mongoose";
+const { Schema } = mongoose;
 
 const BookingSchema = new Schema({
     studySession: {
--- server/models/Review.js
+++ server/models/Review.js
@@ -1,4 +1,5 @@
-import Schema from 'mongoose';
+import mongoose from "mongoose";
+const { Schema } = mongoose;
 
 const ReviewSchema = new Schema({
     booking: {
