e76ac163153b88ca03134df3240e1d9929c7a2d0
peytonlandyouknow <fabian_leibinger@gmx.de>
2023-05-18 23:31:30 +0200

    UniversityController implemented

--- server/controllers/UniversityController.js
+++ server/controllers/UniversityController.js
@@ -1,56 +1,98 @@
 import University from '../models/University.js';
-import createError from '../utils/createError.js';
 import { ObjectId } from 'mongodb';
 
-export const createUniversity = async (req, res, next) => {
+export const createUniversity = async (req, res) => {
     try {
         const newUniversity = new University({
             name: req.body.name,
             country: req.body.country,
         });
-
-        const savedUniversity = await newUniversity.save();
-        res.status(201).send(savedUniversity);
+        newUniversity.save(newUniversity)
+            .then(university => {
+                res.status(201).send(university);
+            }).catch(err => {
+                res.status(500).send('Failed to create object!');
+            });
     } catch (err) {
-        next(err);
+        res.status(400).send('Bad request!');
     }
 };
 
-export const getUniversity = async (req, res, next) => {
+export const getUniversity = async (req, res) => {
+    var universityId;
     try {
-        const university = await University.find({ _id: new ObjectId(req.params.universityId) });
-        res.status(200).send(university);
+        universityId = new ObjectId(req.params.universityId);
+        University.findById(universityId)
+            .then(university => {
+                if (!university) {
+                    res.status(404).send('University not found!');
+                } else {
+                    res.status(200).send(university);
+                }
+            }).catch(err => {
+                res.status(500).send('Failed to retrieve university!');
+            });
     } catch (err) {
-        next(err);
+        res.status(400).send('Bad request!');
     }
 };
-export const deleteUniversity = async (req, res, next) => {
+
+export const getUniversities = async (req, res) => {
+    University.find()
+        .then(universities => {
+            if (universities.length === 0) {
+                res.status(404).send('No universities found!');
+            } else {
+                res.status(200).send(universities);
+            }
+        }).catch(err => {
+            res.status(500).send('Internal server error!');
+        });
+};
+
+export const updateUniversity = async (req, res) => {
+    var universityId;
+    var updatedUniversity;
     try {
-        const university = await University.find({ _id: new ObjectId(req.params.universityId) });
-        if (university.length === 0) {
-            return next(createError(404, 'University not found!'));
-        }
-        await University.findByIdAndDelete({ _id: new ObjectId(req.params.universityId) });
-        res.status(200).send('deleted.');
+        universityId = new ObjectId(req.params.universityId);
+        updatedUniversity = new University({
+            name: req.body.name,
+            country: req.body.country,
+        });
+        University.findByIdAndUpdate(universityId,
+            {
+                name: updatedUniversity.name,
+                country: updatedUniversity.country,
+            })
+            .then(university => {
+                if (!university) {
+                    res.status(404).send('University not found!');
+                } else {
+                    res.status(200).send(updatedUniversity);
+                }
+            }).catch(err => {
+                res.status(500).send('Failed to update university!');
+            });
     } catch (err) {
-        next(err);
+        res.status(400).send('Bad request!');
     }
 };
-// TO DO: Implement this
-export const updateUniversity = async (req, res, next) => {
+
+export const deleteUniversity = async (req, res) => {
+    var universityId;
     try {
-        const university = await University.find({ _id: new ObjectId(req.params.universityId) });
-        if (university.length === 0) {
-            return next(createError(404, 'University not found!'));
-        }
-        const updatedUniversity = await University.findByIdAndUpdate(
-            req.params.universityId,
-            req.body,
-            { name: req.body.name,
-                country: req.body.country}
-        );
-        res.status(200).send(updatedUniversity);
+        universityId = new ObjectId(req.params.universityId);
+        University.findByIdAndDelete(universityId)
+            .then(university => {
+                if (!university) {
+                    res.status(404).send('University not found!');
+                } else {
+                    res.status(200).send('University deleted.');
+                }
+            }).catch(err => {
+                res.status(500).send('Failed to delete university!');
+            });
     } catch (err) {
-        next(err);
+        res.status(400).send('Bad request!');
     }
 };
--- server/routes/UniversityRoute.js
+++ server/routes/UniversityRoute.js
@@ -2,15 +2,17 @@ import express from 'express';
 import {
   createUniversity,
   getUniversity,
+  getUniversities,
   deleteUniversity,
   updateUniversity,
 } from '../controllers/UniversityController.js';
 
 const router = express.Router();
 
-router.post('/', createUniversity );
+router.post('/', createUniversity);
 router.get('/:universityId', getUniversity);
-router.delete('/:universityId', deleteUniversity);
+router.get('/', getUniversities);
 router.put('/:universityId', updateUniversity);
+router.delete('/:universityId', deleteUniversity);
 
 export default router;
