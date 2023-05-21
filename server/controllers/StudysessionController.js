8aedef26231690aeddb9c68a0d21c95643367641
peytonlandyouknow <fabian_leibinger@gmx.de>
2023-05-21 22:25:47 +0200

    Created studysession controller

---
+++ server/controllers/StudysessionController.js
@@ -0,0 +1,144 @@
+import Course from '../models/Course.js';
+import Studysession from '../models/Studysession.js';
+import User from '../models/User.js';
+import { ObjectId } from 'mongodb';
+
+export const createStudysession = async (req, res) => {
+    try {
+        // Check if studysession from the same tutor exists for the same course.
+        const existingStudysession = await Studysession.findOne({
+            course: req.body.course,
+            tutoredBy: req.body.tutoredBy,
+        });
+        if (existingStudysession) {
+            res.status(409).send('Object already exists!');
+            return;
+        }
+        // Check if course exists.
+        const courseId = new ObjectId(req.body.course);
+        const course = await Course.findById(courseId);
+        if (!course) {
+            res.status(404).send('Object reference not found!');
+            return;
+        }
+        // Check if tutor exists.
+        const userId = new ObjectId(req.body.tutoredBy);
+        const user = await User.findById(userId);
+        if (!user) {
+            res.status(404).send('Object reference not found!');
+            return;
+        }
+        // Create studysession.
+        const newStudysession = new Studysession({
+            course: courseId,
+            tutoredBy: userId,
+            description: req.body.description,
+            pricePerHourEuro: req.body.pricePerHourEuro,
+            languages: req.body.languages,
+        });
+        try {
+            const savedStudysession = await newStudysession.save();
+            res.status(201).send(savedStudysession);
+        } catch (err) {
+            res.status(500).send('Failed to create object!');
+        }
+    } catch (err) {
+        res.status(400).send('Bad request!');
+    }
+};
+
+export const getStudysessions = async (req, res) => {
+    try {
+        const studysessions = await Studysession.find();
+        if (!studysessions) {
+            res.status(404).send('No studysessions found!');
+        } else {
+            res.status(200).send(studysessions);
+        }
+    } catch (err) {
+        res.status(500).send('Failed to retrieve studysessions!');
+    }
+};
+
+export const getStudysession = async (req, res) => {
+    try {
+        const studysessionId = new ObjectId(req.params.studysessionId);
+        const studysession = await Studysession.findById(studysessionId);
+        try {
+            if (!studysession) {
+                res.status(404).send('Studysession not found!');
+            } else {
+                res.status(200).send(studysession);
+            }
+        } catch (err) {
+            res.status(500).send('Failed to retrieve studysession!');
+        }
+    } catch (err) {
+        res.status(400).send('Bad request!');
+    }
+};
+
+export const updateStudysession = async (req, res) => {
+    try {
+        // Check if course exists.
+        const courseId = new ObjectId(req.body.course);
+        const course = await Course.findById(courseId);
+        if (!course) {
+            res.status(404).send('Object reference not found!');
+            return;
+        }
+        // Check if tutor exists.
+        const userId = new ObjectId(req.body.tutoredBy);
+        const user = await User.findById(userId);
+        if (!user) {
+            res.status(404).send('Object reference not found!');
+            return;
+        }
+        // Update studysession.
+        const studysessionId = new ObjectId(req.params.studysessionId);
+        const updatedStudysession = new Studysession({
+            course: courseId,
+            tutoredBy: userId,
+            description: req.body.description,
+            pricePerHourEuro: req.body.pricePerHourEuro,
+            languages: req.body.languages,
+        });
+        try {
+            const studysession = await Studysession.findByIdAndUpdate(studysessionId, 
+                {
+                    name: updatedStudysession.name,
+                    tutoredBy: updatedStudysession.tutoredBy,
+                    description: updatedStudysession.description,
+                    pricePerHourEuro: updatedStudysession.pricePerHourEuro,
+                    languages: updatedStudysession.languages,
+                });
+            if (!studysession) {
+                res.status(404).send('Studysession not found!');
+            } else {
+                res.status(200).send(updatedStudysession);
+            }
+        } catch (err) {
+            res.status(500).send('Failed to update studysession!');
+        }
+    } catch (err) {
+        res.status(400).send('Bad request!');
+    }
+};
+
+export const deleteStudysession = async (req, res) => {
+    try {
+        const studysessionId = new ObjectId(req.params.studysessionId);
+        try {
+            const studysession = await Studysession.findByIdAndDelete(studysessionId);
+            if (!studysession) {
+                res.status(404).send('Studysession not found!');
+            } else {
+                res.status(200).send('Studysession deleted!');
+            }
+        } catch (err) {
+            res.status(500).send('Failed to delete studysession!');
+        }
+    } catch (err) {
+        res.status(400).send('Bad request!');
+    }
+}
--- server/routes/StudysessionRoute.js
+++ server/routes/StudysessionRoute.js
@@ -1,5 +1,12 @@
 import express from 'express';
+import { createStudysession, getStudysessions, getStudysession, updateStudysession, deleteStudysession } from '../controllers/StudysessionController.js';
 
 const router = express.Router();
+
+router.post('/', createStudysession);
+router.get('/', getStudysessions);
+router.get('/:studysessionId', getStudysession);
+router.put('/:studysessionId', updateStudysession);
+router.delete('/:studysessionId', deleteStudysession);
 
 export default router;
