1940e811a99ae70d2e2f31106a9d21be52e18992
ge83gig <fabian.leibinger@tum.de>
2023-06-07 17:14:46 +0200

    apparently we dont need context when using useQuery

--- client/src/components/MyChats.js
+++ client/src/components/MyChats.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { getChatsOfUser } from '../api/Chat';
 import { useQuery } from 'react-query';
-import { Box, Skeleton, Snackbar, Alert } from '@mui/material';
+import { Box, Skeleton, Alert } from '@mui/material';
 
 const MyChats = () => {
+
+    const userId = '6468f36705853e6071dfec63';
+    // TODO: Move somewhere else after userid is retrieved from auth.
+    const { isLoading, error, data } = useQuery(['chats'], () => getChatsOfUser(userId));
 
     const boxSx = {
         display: 'flex',
@@ -15,12 +19,8 @@ const MyChats = () => {
         width: 0.5,
     };
 
-    const loading_skeleton = <Skeleton variant='rounded' sx={{ flexGrow: 1, width: 1, marginBottom: '3vh' }} />
+    const loading_skeleton = <Skeleton variant='rounded' sx={{ flexGrow: 1, width: 1, marginBottom: '3vh' }} />;
 
-    const [open, setOpen] = React.useState(false);
-
-    const userId = '6468f36705853e6071dfec63';
-    const { isLoading, error, data } = useQuery(['chats'], () => getChatsOfUser(userId));
     if (isLoading) return (
         <Box sx={boxSx}>
             {loading_skeleton}
@@ -31,6 +31,7 @@ const MyChats = () => {
             {loading_skeleton}
         </Box>
     );
+
     if (error) return (
         <Box sx={boxSx}>
             <Alert severity='info' sx={{ flexGrow: 1, width: 0.95 }}>
@@ -38,15 +39,10 @@ const MyChats = () => {
             </Alert>
         </Box>
     );
-    const chats = data;
-    console.log(chats);
+
     return (
         <Box sx={boxSx}>
-            {loading_skeleton}
-            {loading_skeleton}
-            {loading_skeleton}
-            {loading_skeleton}
-            {loading_skeleton}
+            
         </Box>
     );
 };
