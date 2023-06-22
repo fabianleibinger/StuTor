e9a556093927982beab73dedf9fb68d3ca12f5c8
Sophie <sophie.witt@tum.de>
2023-06-22 17:56:48 +0200

    Add payment schema

---
+++ server/models/Payment.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const PaymentSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+        unique: true,
+    },
+    customerId: {
+        type: String,
+        required: true,
+    }
+});
+
+const Payment = mongoose.model('Payment', PaymentSchema);
+
+export default Payment;
