f6639a049810ae7eda234c1310108880d0c3c295
Sophie <sophie.witt@tum.de>
2023-07-05 15:28:06 +0200

    populate studysession and createdBy and format table

--- client/src/components/ViewBookings/BookingTable.js
+++ client/src/components/ViewBookings/BookingTable.js
@@ -13,29 +13,42 @@ import Typography from '@mui/material/Typography';
 import Paper from '@mui/material/Paper';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import { Avatar, Button } from '@mui/material';
+import Grid from '@mui/material/Grid';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import CancelIcon from '@mui/icons-material/Cancel';
 
-function createData(name, calories, fat, carbs, protein, price) {
-  return {
-    name,
-    calories,
-    fat,
-    carbs,
-    protein,
-    price,
-    history: [
-      {
-        date: '2020-01-05',
-        customerId: '11091700',
-        amount: 3,
-      },
-      {
-        date: '2020-01-02',
-        customerId: 'Anonymous',
-        amount: 1,
-      },
-    ],
-  };
+export default function BookingTable(bookings) {
+    console.log("bookings in table", bookings.children[1])
+    return (
+        <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 700 }} aria-label="collapsible table">
+            <TableHead>
+            <TableRow>
+                <TableCell />
+                <TableCell>Studysession</TableCell>
+                <TableCell>Student</TableCell>
+                <TableCell>Booking date</TableCell>
+                <TableCell>Is confirmed by student</TableCell>
+                <TableCell>Rating</TableCell>
+                <TableCell>Need help?</TableCell>
+            </TableRow>
+            </TableHead>
+            <TableBody>
+            {bookings.children[1].map((booking) => (
+                <Row key={booking._id} row={booking} />
+            ))}
+            </TableBody>
+        </Table>
+        </TableContainer>
+    );
 }
+
+const formatDate = (dateString) => {
+  const date = new Date(dateString);
+  const formattedDate = date.toLocaleDateString('en-GB');
+  return formattedDate;
+};
 
 function Row(props) {
   const { row } = props;
@@ -54,53 +67,35 @@ function Row(props) {
           </IconButton>
         </TableCell>
         <TableCell component="th" scope="row">
-          {row.name}
+          {row.studysession.course.name}
         </TableCell>
-        <TableCell align="right">{row.calories}</TableCell>
-        <TableCell align="right">{row.fat}</TableCell>
-        <TableCell align="right">{row.carbs}</TableCell>
-        <TableCell align="right">{row.protein}</TableCell>
-      </TableRow>
-      <TableRow>
-        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
-          <Collapse in={open} timeout="auto" unmountOnExit>
-            <Box sx={{ margin: 1 }}>
-              <Typography variant="h6" gutterBottom component="div">
-                History
-              </Typography>
-              <Table size="small" aria-label="purchases">
-                <TableHead>
-                  <TableRow>
-                    <TableCell>Date</TableCell>
-                    <TableCell>Customer</TableCell>
-                    <TableCell align="right">Amount</TableCell>
-                    <TableCell align="right">Total price ($)</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {row.history.map((historyRow) => (
-                    <TableRow key={historyRow.date}>
-                      <TableCell component="th" scope="row">
-                        {historyRow.date}
-                      </TableCell>
-                      <TableCell>{historyRow.customerId}</TableCell>
-                      <TableCell align="right">{historyRow.amount}</TableCell>
-                      <TableCell align="right">
-                        {Math.round(historyRow.amount * row.price * 100) / 100}
-                      </TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </Box>
-          </Collapse>
+        <TableCell>
+            <Grid container alignItems='center' spacing={2}>
+                <Grid item>
+                <Avatar src={row.createdBy.picture} />
+                </Grid>
+                <Grid item>
+            <span>{row.createdBy.firstname} {row.createdBy.lastname}</span>
+                </Grid>
+            </Grid>
+            </TableCell>
+        <TableCell>{formatDate(row.createdAt)}</TableCell>
+        <TableCell>
+            {row.isConfirmed && (
+            <CheckCircleIcon style={{ color: 'green' }}/>
+        )}
+        {!row.isConfirmed && (
+            <CancelIcon style={{ color: 'red' }}/>
+        )}
         </TableCell>
+        <TableCell>{row.reviewGiven}</TableCell>
+        <TableCell><Button>Contact customer support</Button></TableCell>
       </TableRow>
     </React.Fragment>
   );
 }
 
-Row.propTypes = {
+/**Row.propTypes = {
   row: PropTypes.shape({
     calories: PropTypes.number.isRequired,
     carbs: PropTypes.number.isRequired,
@@ -116,17 +111,9 @@ Row.propTypes = {
     price: PropTypes.number.isRequired,
     protein: PropTypes.number.isRequired,
   }).isRequired,
-};
+};**/
 
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0, 3.99),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3, 4.99),
-  createData('Eclair', 262, 16.0, 24, 6.0, 3.79),
-  createData('Cupcake', 305, 3.7, 67, 4.3, 2.5),
-  createData('Gingerbread', 356, 16.0, 49, 3.9, 1.5),
-];
-
-export default function CollapsibleTable() {
+function CollapsibleTable() {
   return (
     <TableContainer component={Paper}>
       <Table aria-label="collapsible table">
