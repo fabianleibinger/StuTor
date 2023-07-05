5e86576f2e790f489ff762a3750ac7d2eca7ca55
Sophie <sophie.witt@tum.de>
2023-07-05 14:30:55 +0200

    call bookings endpoint from frontend

--- client/src/pages/ViewBookingsPage.js
+++ client/src/pages/ViewBookingsPage.js
@@ -1,11 +1,36 @@
 import CollapsibleTable from "../components/ViewBookings/CollapsibleTable";
+import React from "react";
+import { useQuery } from "react-query";
+import { getBookingsOfTutor } from "../api/Booking";
+import getCurrentUser from "../utils/getCurrentUser";
 
 const ViewBookingsPage = () => {
+    //const user = getCurrentUser();
+    //const userId = user._id;
+    const userId = "6468f36705853e6071dfec63"
+
+    const { isLoading, error, data: bookings } = useQuery(["tutorBookings"], () => getBookingsOfTutor(userId));
+
+    if (isLoading) return "Loading...";
+    if (error) return "An error has occurred: " + error.message;
+    console.log(error)
+    console.log(isLoading)
+    console.log(bookings);
 
     return (
         <div>
             <h1>View Bookings</h1>
             <CollapsibleTable />
+            {bookings.map((booking) => (
+                <div key={booking._id}>
+                    <p>Booking ID: {booking._id}</p>
+                    <p>Study Session ID: {booking.studysession}</p>
+                    <p>Created By: {booking.createdBy}</p>
+                    <p>Hours: {booking.hours}</p>
+                    <p>Is Payed: {booking.isPayed}</p>
+                    <p>Is Confirmed: {booking.isConfirmed}</p>
+                    </div>
+            ))}
         </div>
     );
 
