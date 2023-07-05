fd0993626b6c56e14dffa933e79e8f1138a96000
Sophie <sophie.witt@tum.de>
2023-07-05 13:49:55 +0200

    Add dummy collapsable table

--- client/src/pages/ViewBookingsPage.js
+++ client/src/pages/ViewBookingsPage.js
@@ -1,8 +1,11 @@
+import CollapsibleTable from "../components/ViewBookings/CollapsibleTable";
+
 const ViewBookingsPage = () => {
 
     return (
         <div>
             <h1>View Bookings</h1>
+            <CollapsibleTable />
         </div>
     );
 
