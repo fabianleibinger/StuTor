1fd76b46d8d3efacafeff3b30e58b71f4fc3a181
Jiacheng Wen <jason.wen@tum.de>
2023-07-06 10:23:21 +0200

    Choose tutor student page, a new register page design

--- client/src/styles.js
+++ client/src/styles.js
@@ -1,13 +1,18 @@
 import { styled } from "@mui/system";
 import {
-  AppBar,
   Button,
   Card,
-  Container,
-  IconButton,
   TextField,
-  Toolbar,
   Typography,
+  IconButton,
+  AppBar,
+  Toolbar,
+  Autocomplete,
+  InputLabel,
+  Stepper,
+  Step,
+  StepLabel,
+  StepIcon,
 } from "@mui/material";
 import { Link } from "react-router-dom";
 import { createTheme } from "@mui/material";
@@ -52,21 +57,43 @@ export const LinksContainer = styled("div")(({ theme }) => ({
   display: "flex",
   alignItems: "center",
 }));
-export const FormContainer = styled("form")(({ theme }) => ({
+export const LoginFormContainer = styled("form")(({ theme }) => ({
   display: "flex",
   flexDirection: "column",
   alignItems: "center",
-  // padding: theme.spacing(3),
+  justifyContent: "center", // Center items vertically
   backgroundColor: "#fff",
-  borderRadius: "4px",
-  boxShadow: "0px 3px 6px rgba(0, 0, 0, 0.16)",
-  minWidth: "400px",
-  minHeight: "400px",
-  // Added spacing between child elements
+  borderRadius: "30px",
+  boxShadow: "0px 10px 16px rgba(0, 0, 0, 0.2)", // Increased shadow values
+  minWidth: "500px",
+  minHeight: "600px",
+  padding: theme.spacing(5), // Add padding around all items
+
   "& > *": {
     marginBottom: theme.spacing(2),
     width: "100%",
   },
+  "& > *:nth-child(2)": {
+    marginTop: theme.spacing(5), // Adjust the value as needed
+  },
+}));
+
+export const FormContainer = styled("form")(({ theme }) => ({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  backgroundColor: "#fff",
+  borderRadius: "30px",
+  boxShadow: "0px 10px 16px rgba(0, 0, 0, 0.2)", // Increased shadow values
+  width: "800px",
+  height: "1000px",
+  padding: theme.spacing(5), // Add padding around all items
+
+  "& > *": {
+    marginBottom: theme.spacing(2),
+  },
+  // Stretch all items to fill the available vertical space
+  flexGrow: 1,
 }));
 
 // -------------------------- Titles --------------------------
@@ -78,13 +105,14 @@ export const CardTitle = styled(Typography)({
 });
 export const LoginTitle = styled(Typography)(({ theme }) => ({
   marginBottom: theme.spacing(10),
-  fontSize: "30px", // Increase the font size to make it bigger
-  fontWeight: "bold", // Apply a bold font weight for emphasis
-  textAlign: "center", // Center-align the text
-  color: theme.palette.primary.contrastText, // Apply a custom color if desired
-  background: theme.palette.primary.main, // Apply a background color from the theme
-  paddingTop: theme.spacing(2), // Add padding to the title
-  paddingBottom: theme.spacing(2), // Add padding to the title
+  fontSize: "30px",
+  fontWeight: "bold",
+  textAlign: "center",
+  color: theme.palette.primary.contrastText,
+  background: theme.palette.primary.main,
+  paddingTop: theme.spacing(2),
+  paddingBottom: theme.spacing(2),
+  width: "100%",
 }));
 
 // -------------------------- Buttons --------------------------
@@ -92,9 +120,9 @@ export const SubmitButton = styled(Button)(({ theme }) => ({
   marginTop: theme.spacing(10),
   marginBottom: theme.spacing(5),
   textTransform: "none",
-  fontSize: "24px", // Adjust the font size to make it slightly smaller
+  fontSize: "24px",
   fontWeight: "bold",
-  padding: `${theme.spacing(0.8)} ${theme.spacing(1.7)}`, // Adjust the padding to make it slightly smaller
+  padding: `${theme.spacing(0.8)} ${theme.spacing(1.7)}`,
   borderRadius: "50px",
   width: "70%",
 }));
@@ -173,5 +201,65 @@ export const UserFullName = styled("div")(({ theme }) => ({
 export const ErrorMessage = styled(Typography)(({ theme }) => ({
   color: "red",
   marginTop: theme.spacing(2),
-  textAlign: "center", // Center-align the error message
+  textAlign: "center",
 }));
+
+export const AutocompleteWrapper = styled(Autocomplete)(({ theme }) => ({
+  "& .MuiAutocomplete-paper.centered-dropdown": {
+    marginTop: theme.spacing(1),
+  },
+}));
+
+export const ProfilePicInputLabel = styled(InputLabel)`
+  margin-top: 10px;
+  margin-bottom: 10px;
+  width: 80%;
+`;
+
+export const ProgressContainer = styled(Stepper)(({ theme }) => ({
+  width: "100%",
+  backgroundColor: "transparent",
+  padding: theme.spacing(2),
+
+  "& .MuiStepIcon-root": {
+    color: "gray",
+    "&.MuiStepIcon-active": {
+      color: theme.palette.primary.main,
+    },
+    /* Add the following styles for completed steps */
+    "&.MuiStepIcon-completed": {
+      color: "green",
+    },
+  },
+}));
+
+export const CustomStepIcon = styled(StepIcon)(({ theme }) => ({
+  "& .MuiStepIcon-root": {
+    fontSize: theme.spacing(10), // Adjust the size as needed
+  },
+}));
+
+export const CustomStep = styled(Step)(({ theme }) => ({
+  "& .MuiStepIcon-root": {
+    width: theme.spacing(4),
+    height: theme.spacing(4),
+  },
+}));
+
+export const CustomStepLabel = styled(StepLabel)(({ theme }) => ({
+  "& .MuiStepLabel-iconContainer": {
+    "& > svg": {
+      fontSize: theme.spacing(4), // Adjust the size as needed
+    },
+  },
+}));
+
+export const stepContentContainer = {
+  // display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  width: "500px",
+  height: "600px",
+  justifyContent: "center",
+  // marginBottom: "50px", // Adjust this margin based on your preference
+};
