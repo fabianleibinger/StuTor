a08efce0c1f012ac2f20904bf3c8b35bd9ded1f7
JasonWen24 <jasonwendavidson@gmail.com>
2023-07-08 23:43:53 +0200

    feat: user profile page styling

---
+++ client/src/components/Explore/Explore.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Explore = () => {
+  return (
+    <div className="explore">
+      <div className="container">
+        <h1>Find Your Tutors</h1>
+        <div className="items">
+          <div className="item">
+            {/* TO-DO TUTOR NAME, TUTOR PROFILE PIC, TUTOR DESCRIPTIONS */}
+            <img
+              src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/graphics-design.d32a2f8.svg"
+              alt=""
+            />
+            <div className="line"></div>
+            <span>TUTOR 1</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Explore;
