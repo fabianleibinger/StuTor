f263ee4209d2d3ace9ee33ecdb6de79badc280ff
Sophie <sophie.witt@tum.de>
2023-07-10 11:17:06 +0200

    Style Booking History

--- client/src/components/Booking/BookingHistoryDialog.js
+++ client/src/components/Booking/BookingHistoryDialog.js
@@ -6,6 +6,16 @@ import {
   DialogContent,
   Grid,
   Typography,
+  TableContainer,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+  Paper,
+  Icon,
+  Tooltip,
+  Alert,
 } from "@mui/material";
 import { useQuery, useMutation, useQueryClient } from "react-query";
 import { confirmBooking as confirmBookingCall, getBookingsOfStudysessionCreatedByUser } from "../../api/Booking.js";
@@ -14,6 +24,8 @@ import { WidthNormal } from "@mui/icons-material";
 import IconButton from "@mui/material/IconButton";
 import RateReviewIcon from '@mui/icons-material/RateReview';
 import CheckIcon from '@mui/icons-material/Check';
+import RecommendIcon from '@mui/icons-material/Recommend';
+import HourglassEmptyIcon from '@mui/icons-material/HourglassEmpty';
 
 const BookingHistoryDialog = ({ open, onClose, userId, studySessionId }) => {
   const [openReviewDialog, setOpenReviewDialog] = useState(false);
@@ -72,45 +84,76 @@ const BookingHistoryDialog = ({ open, onClose, userId, studySessionId }) => {
           {isloading && <div>Loading...</div>}
           {error && <div>No bookings found!</div>}
           {!isloading && !error && (
-            <Grid container spacing={4} direction={'column'}>
-              {bookings.map((booking) => (
-                booking.isPayed && (
-                <Grid container item key={booking._id} direction={'row'} spacing={1}>
-                <Grid item key={booking._id} xs={20}>
-                  <Typography variant="body1">
-                    { booking.hours < 2 ? booking.hours + " hour" : booking.hours + " hours"}
-                  </Typography>
-                  <Typography variant="body2">
-                  booked on {formatDate(booking.createdAt)}
-                  </Typography>
-                  </Grid>
-                  {booking.isPayed && !booking.isConfirmed && (
-                    <Grid item key={booking._id} justifyContent={'flex-end'}>
+            <div>
+              <div style={{ paddingBottom: '1rem' }}>
+            <Alert severity="info">Here you can see all your bookings for this studysession and have the possibility to confirm your booking as well as rate your tutor.</Alert>
+            </div>
+            <TableContainer component={Paper}>
+             <Table aria-label="simple table">
+               <TableHead>
+                 <TableRow>
+                   <TableCell>Status</TableCell>
+                   <TableCell>Hours</TableCell>
+                   <TableCell>Booking date</TableCell>
+                   <TableCell>Action</TableCell>
+                 </TableRow>
+               </TableHead>
+               <TableBody>
+                 {bookings.map((booking) => (
+                  booking.isPayed && (
+                    <TableRow
+                    key={booking._id}
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  >
+                     <TableCell>
+                      {booking.isPayed && !booking.isConfirmed && (
+                        <Icon>
+                          <HourglassEmptyIcon color="warning"/>
+                          </Icon>
+                          )}
+                      {booking.isConfirmed && booking.reviewGiven && (
+                        <Icon>
+                          <RecommendIcon color="success"/>
+                        </Icon>
+                      )}
+                      {booking.isConfirmed && !booking.reviewGiven && (
+                        <Icon>
+                          <CheckIcon color="primary"/>
+                        </Icon>
+                      )}
+
+                        </TableCell>
+                     <TableCell>{booking.hours}</TableCell>
+                     <TableCell>{formatDate(booking.createdAt)}</TableCell>
+                     <TableCell>
+                     {booking.isConfirmed && !booking.reviewGiven && (
+                      <Tooltip title="Rate your tutor">
+                     <IconButton
+                        aria-label="give review"
+                        onClick={() => handleGiveReview(booking._id)}
+                      >
+                        <RateReviewIcon />
+                      </IconButton>
+                      </Tooltip>
+                      )}
+                      {booking.isPayed && !booking.isConfirmed && (
+                        <Tooltip title="Confirm that studysession took place">
                       <IconButton
                         aria-label="confirm booking"
                         onClick={() => handleConfirm(booking._id)}
                       >
                         <CheckIcon />
                       </IconButton>
-                    </Grid>
+                      </Tooltip>
                   )}
-                  {booking.isConfirmed && !booking.reviewGiven && (
-                    <Grid item key={booking._id} alignContent={'center'}>
-                      <div>
-                      <IconButton
-                        aria-label="give review"
-                        onClick={() => handleGiveReview(booking._id)}
-                      >
-                        <RateReviewIcon />
-                      </IconButton>
-                      </div>
-                    </Grid>
-                  )}
-                  </Grid>
-                )
-              ))}
-            </Grid>
-
+                     </TableCell>
+                   </TableRow>
+                  )
+                 ))}
+               </TableBody>
+             </Table>
+           </TableContainer>
+           </div> 
           )}
         </DialogContent>
       </Dialog>
