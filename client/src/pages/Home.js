b5667bee82432d2a67a448eac75b6fc81e005840
JasonWen24 <jasonwendavidson@gmail.com>
2023-07-10 11:00:57 +0200

    home page changes

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Home.scss";
 import SearchBar from "../components/SearchBar/SearchBar";
-import TrustedBy from "../components/trustedBy/TrustedBy";
+import TrustedBy from "../components/TrustedBy/TrustedBy";
 import Features from "../components/Features/Features";
 import Slide from "../components/slide/Slide";
 import ProjectCard from "../components/projectCard/ProjectCard";
