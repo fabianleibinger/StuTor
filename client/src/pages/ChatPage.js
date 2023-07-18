0876974bd1d237a6bb996387f2d8b0c5768cb752
peytonlandyouknow <fabian_leibinger@gmx.de>
2023-07-18 18:01:03 +0200

    chatpage working

--- client/src/pages/ChatPage.js
+++ client/src/pages/ChatPage.js
@@ -1,4 +1,4 @@
-import Box from "@mui/material/Box";
+import { Box, Grid, useMediaQuery, useTheme } from "@mui/material";
 import MyChats from "../components/Chat/MyChats";
 import ChatBox from "../components/Chat/ChatBox";
 
@@ -7,38 +7,60 @@ import ChatBox from "../components/Chat/ChatBox";
  * Incorporates the MyChats and ChatBox components.
  */
 const ChatPage = () => {
+
+  // Check if the screen width is below the breakpoint (sm or xs)
+  const theme = useTheme();
+  const isSmScreen = useMediaQuery(theme.breakpoints.down("sm"));
+  const isXsScreen = useMediaQuery(theme.breakpoints.down("xs"));
+
   return (
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: "row",
-        flexWrap: "wrap",
-        justifyContent: "space-between",
-        alignItems: "stretch",
-        alignContent: "stretch",
-        width: "97vw",
-        height: "90vh",
-        mx: "auto",
-        marginBottom: "1vh",
-      }}
-    >
-      <Box width={0.49} height={1} sx={{marginTop: "2vh"}}>
-        {<MyChats />}
-      </Box>
-      <Box
-        width={0.45}
-        height={1}
-        sx={{
-          boxShadow: "0px 10px 16px rgba(0, 0, 0, 0.2)",
-          borderRadius: "30px",
-          padding: "1rem",
-          margin: "2rem",
-          height: "85vh",
-        }}
+    <Grid container spacing={2}>
+      <Grid
+        item
+        xs={12}
+        sm={isSmScreen ? 12 : 6}
+        md={isXsScreen ? 12 : 6}
+        lg={6}
       >
-        {<ChatBox />}
-      </Box>
-    </Box>
+        {/* Course Details */}
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            backgroundColor: "#f5f5f5",
+            borderRadius: "30px",
+            flexWrap: "wrap",
+            boxShadow: "0px 10px 16px rgba(0, 0, 0, 0.2)", // Increased shadow values
+            padding: "1rem",
+            margin: "2rem",
+            overflow: "auto",
+            height: "85vh", // Set a specific height to limit vertical scrolling
+          }}
+        >
+          {<MyChats />}
+        </Box>
+      </Grid>
+
+      <Grid
+        item
+        xs={12}
+        sm={isSmScreen ? 12 : 6}
+        md={isXsScreen ? 12 : 6}
+        lg={6}
+      >
+        <Box
+          sx={{
+            boxShadow: "0px 10px 16px rgba(0, 0, 0, 0.2)", // Increased shadow values
+            borderRadius: "30px",
+            padding: "1rem",
+            margin: "2rem",
+            height: "85vh",
+          }}
+        >
+          {<ChatBox />}
+        </Box>
+      </Grid>
+    </Grid>
   );
 };
 
